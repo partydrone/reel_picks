3366b3a7615bfd829a3b0a6083d3a3f767179d58
Andrew Porter <partydrone@icloud.com>
2025-01-31 11:46:31 -0600

    Implement user profile
    Make it so users can edit their information, including avatar, name, email, and password.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
-  layout "authentication"
+  before_action :set_user, only: %i[ edit update ]
+
+  layout "authentication", only: %i[ new create ]
 
   # GET /users/new
   def new
     @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
   end
 
   # POST /users or /users.json
@@ -25,9 +31,26 @@ class UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to edit_user_path(@user), notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+  def set_user
+    @user = User.find(params.expect(:id))
+  end
+
   def user_params
-    params.expect(user: [ :first_name, :last_name, :email_address, :password, :password_confirmation ])
+    params.expect(user: [ :first_name, :last_name, :email_address, :password, :password_confirmation, :avatar ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :movies
   resource :session
   resources :passwords, param: :token
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new edit create update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
