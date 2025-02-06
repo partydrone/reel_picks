12496631a53d5463ed13922833f40b26a4185d05
Andrew Porter <partydrone@icloud.com>
2025-02-06 10:30:24 -0600

    Update straggling Tailwind classes to v4.0

--- app/helpers/ui_helper.rb
+++ app/helpers/ui_helper.rb
@@ -1,6 +1,6 @@
 module UiHelper
   def page_header(text)
-    tag.header(class: "bg-white shadow-sm") do
+    tag.header(class: "bg-white shadow-xs") do
       tag.div(class: "mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8") do
         tag.h1(text, class: "text-lg font-semibold text-stone-900")
       end
